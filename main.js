756a2db24cd8970b5afc4de0cf115fd6f7cfe89b
DaiwikDiwani <74300290+DaiwikDiwani@users.noreply.github.com>
2021-09-03 15:48:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+console.log ("Hi I am Console screen");
+var fname="Daiwik";
+console.log(fname);
+
+var lname="Diwani";
+console.log(lname);
+
+var fullname=fname.concat(lname);
+console.log(fullname);
+
+var square_root=Math.sqrt(36);
+console.log (square_root);
+
+var max_number=Math.max(89,62);
+console.log (max_number);
+
+var min_number=Math.min(5,800);
+console.log (min_number);
+
+var power_number=Math.pow(60,2);
+console.log (power_number);
+
+var power_number=Math.pow(55,2);
+console.log (power_number);
+
+var a = 10;
+var b = 5;
+
+var add = a+b ;
+console.log(add);
+
+var multiple = a*b;
+console.log(multiple);
+
+var divide = a / b ;
+
+
+
+
